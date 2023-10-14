a30e1321784b12c203bc7166bce08c5ed14da265
Mohamed ELMesery <mohhamedelmesery225500@gmail.com>
2023-10-14 17:15:49 +0300

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,31 +11,11 @@
 **/
 int main(void)
 {
-putchar ('a');
-putchar ('b');
-putchar ('c');
-putchar ('d');
-putchar ('e');
-putchar ('f');
-putchar ('g');
-putchar ('h');
-putchar ('i');
-putchar ('j');
-putchar ('k');
-putchar ('l');
-putchar ('m');
-putchar ('n');
-putchar ('o');
-putchar ('p');
-putchar ('q');
-putchar ('r');
-putchar ('s');
-putchar ('t');
-putchar ('u');
-putchar ('v');
-putchar ('w');
-putchar ('x');
-putchar ('y');
-putchar ('z');
+char a = 'a';
+while (a <= 'z')
+{
+putchar(a);
+a++;
+}
 return (0);
 }
