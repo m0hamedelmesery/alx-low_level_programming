ea5b21c85b5f20df5a3621f90949c47afd366dba
m0hamedelmesery <mohhamedelmesery225500@gmail.com>
2023-10-14 19:32:33 +0300

    printcomb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Description: printing compinations
+*
+*Return: 0 (success)
+**/
+
+int main(void)
+{
+int x = 0, y;
+while ( x <= 9)
+{
+y = 0;
+while (y <= 9)
+{
+if (x != y && x < y)
+{
+putchar(x + 48);
+putchar(y + 48);
+
+if (x + y != 17)
+{
+putchar(',');
+putchar(' ');
+}
+}
+y++;
+}
+x++;
+}
+putchar('\n');
+return (0);
+}
