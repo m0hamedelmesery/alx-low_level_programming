1590dbef8f69066ba0d8ef547a99df21e26de645
Mohamed ELMesery <mohhamedelmesery225500@gmail.com>
2023-10-14 17:06:46 +0300

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - entry point
+*
+*Description: false or true validation
+*
+*Return: 0 (success)
+**/
+int main(void)
+{
+putchar ('a');
+putchar ('b');
+putchar ('c');
+putchar ('d');
+putchar ('e');
+putchar ('f');
+putchar ('g');
+putchar ('h');
+putchar ('i');
+putchar ('j');
+putchar ('k');
+putchar ('l');
+putchar ('m');
+putchar ('n');
+putchar ('o');
+putchar ('p');
+putchar ('q');
+putchar ('r');
+putchar ('s');
+putchar ('t');
+putchar ('u');
+putchar ('v');
+putchar ('w');
+putchar ('x');
+putchar ('y');
+putchar ('z');
+return (0);
+}
