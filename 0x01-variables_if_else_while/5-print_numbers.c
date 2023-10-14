3f17eeb1d61ee046423b01954c38bc37e6365f38
m0hamedelmesery <mohhamedelmesery225500@gmail.com>
2023-10-14 17:59:31 +0300

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Description: printing numbers
+*
+*Return: 0 (success)
+**/
+
+int main(void)
+{
+int x = 0;
+while (x <= 9)
+{
+printf("%i", x);
+x++;
+}
+printf("\n");
+return (0);
+}
